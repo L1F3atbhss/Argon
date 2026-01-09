9388a65cb14afd63a9dc4f0dc00d64cb4843ffa4
L1F3atbhss <Nathan.chan3000@gmail.com>
2026-01-09 12:55:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ WIDTH = 1267
 HEIGHT = 775
 FOV = math.pi / 3
 HALF_FOV = FOV / 2
-NUM_RAYS = 180
+NUM_RAYS = 220
 MAX_DEPTH = 750
 DELTA_ANGLE = FOV / NUM_RAYS
-SCALE = WIDTH // NUM_RAYS
+SCALE = WIDTH / NUM_RAYS
 
 # GAME STATES
 MENU = "menu"
 GAME = "game"
 CREDITS = "credits"
 game_state = MENU
+#game_state = GAME
 
 # INIT 
 pygame.init()
@@ -26,32 +27,23 @@ clock = pygame.time.Clock()
 SQW = 1000
 SQH = 1000
 
-#  MAPs
-MAP67 = [
-    *[
-        "".join(["#" for x in range(SQW)]),
-        *[("#" + "".join(["." for x in range(SQW-2)]) + "#") for x in range(SQH)],
-        "".join(["#" for x in range(SQW)]),
-    ]
-]
-
 # Main Map
 MAP =[
     "###########",
     "#......#..#",
-    "#.#$#.....#",
+    "#.#$#....$#",
     "#.###....##",
     "#..#.......",
-    "##....##..#",
-    "##.######.#",
-    "##..#######",
-    "#...#######",
-    "##....###.#",
-    "#.....##..#",
-    "#....####.#",
-    "##........#",
+    "##....##$.#",
+    "##.######$#",
+    "##.$#######",
+    "#$.####$..#",
+    "##...####.#",
+    "#....$##$.#",
+    "###.#####.#",
+    "##$..##...#",
     "###......##",
-    "#######..##"
+    "####..#..##"
 ]
 
 area1 = [
@@ -64,13 +56,12 @@ area1 = [
     "###########"
 ]
 
-
-
 TILE = 100
 MAP_WIDTH = len(MAP[0]) * TILE
 MAP_HEIGHT = len(MAP) * TILE
 
 chests = []
+swords = []
 
 for row_idx, row in enumerate(MAP):
     for col_idx, cell in enumerate(row):
@@ -89,6 +80,9 @@ small_font = pygame.font.SysFont("arial", 24)
 px, py = 150, 150
 angle = 0
 speed = 2
+SWORD_PICKUP_DIST = 40
+has_sword = False
+coins = 0
 
 # Chest
 Chest_size = 255
@@ -97,7 +91,7 @@ Chest_image = pygame.transform.scale(Chest_image, (Chest_size, Chest_size))
 
 # sword
 sword_size = 255
-sword_image = pygame.image.load("Chest.png")
+sword_image = pygame.image.load("sword.png")
 sword_image = pygame.transform.scale(sword_image, (sword_size, sword_size))
 
 def draw_centered_text(text, font, color, y):
@@ -133,6 +127,15 @@ while running:
         else:
             running = False
 
+    for chest in chests[:]:
+        cx, cy = chest
+        if math.hypot(px - cx, py - cy) < 40:
+            chests.remove(chest)
+            swords.append((cx + 20, cy + 20))
+            print("Got Swords")
+            coins = coins + 1
+            print("Coins:" + coins)
+
     # MENU 
     if game_state == MENU:
         screen.fill((10, 10, 20))
@@ -161,6 +164,9 @@ while running:
         clock.tick(60)
         continue
 
+    # zbuffer
+    zbuffer = [0] * NUM_RAYS
+
     # GAME
     dx = math.cos(angle) * speed
     dy = math.sin(angle) * speed
@@ -179,6 +185,13 @@ while running:
     screen.fill((30, 30, 30))
     pygame.draw.rect(screen, (70, 70, 70), (0, HEIGHT // 2, WIDTH, HEIGHT // 2))
 
+    if has_sword:
+        hand_sword = pygame.transform.scale(sword_image, (300, 300))
+        screen.blit(
+            hand_sword,
+            (WIDTH - 300, HEIGHT - 300)
+        )
+
     ray_angle = angle - HALF_FOV
 
     for ray in range(NUM_RAYS):
@@ -189,13 +202,10 @@ while running:
             if is_wall(x, y):
                 depth *= math.cos(angle - ray_angle)
                 wall_height = (50000 / (depth + 0.0001))
+                zbuffer[ray] = depth # store wall depth
 
                 color = 255 / (1 + depth * depth * 0.00002)
-                pygame.draw.rect(
-                    screen,
-                    (color, color, color),
-                    (ray * SCALE, HEIGHT // 2 - wall_height // 2, SCALE, wall_height)
-                )
+                pygame.draw.rect(screen,(color, color, color),(int(ray * SCALE), HEIGHT // 2 - wall_height // 2, int(SCALE + 1), wall_height))
                 break
 
         ray_angle += DELTA_ANGLE
@@ -203,35 +213,73 @@ while running:
     for cx, cy in chests:
         dx = cx - px
         dy = cy - py
-        distance = math.sqrt(dx*dx + dy*dy)
+        distance = math.hypot(dx, dy)
 
         angle_to_chest = math.atan2(dy, dx)
         delta_angle = angle_to_chest - angle
 
-        # Keep angle between -pi and pi
+        # Normalize angle
         while delta_angle > math.pi:
             delta_angle -= 2 * math.pi
         while delta_angle < -math.pi:
             delta_angle += 2 * math.pi
 
-        # Check if chest is inside FOV
+        # Inside FOV
         if -HALF_FOV < delta_angle < HALF_FOV and distance > 30:
             projected_dist = distance * math.cos(delta_angle)
-            chest_size = min(600, int(50000 / projected_dist))
+            chest_size = min(400, int(50000 / projected_dist))
 
-            screen_x = int(
-                (delta_angle + HALF_FOV) / FOV * WIDTH
-            )
+            ray_index = int((delta_angle + HALF_FOV) / FOV * NUM_RAYS)
+            screen_x = ray_index * SCALE
 
-            chest_img = pygame.transform.scale(
-                Chest_image, (chest_size, chest_size)
-            )
+            # Z-buffer check (THIS prevents see-through walls)
+            if 0 <= ray_index < NUM_RAYS and projected_dist < zbuffer[ray_index]:
+                chest_img = pygame.transform.scale(
+                    Chest_image, (chest_size, chest_size)
+                )
 
-            screen.blit(
-                chest_img,
-                (screen_x - chest_size // 2,
-                HEIGHT // 2 - chest_size // 2)
-            )
+                screen.blit(
+                    chest_img,
+                    (screen_x - chest_size // 2, HEIGHT // 2 - chest_size // 2 )
+                )
+                
+    for sword in swords[:]:
+        sx, sy = sword
+        if math.hypot(px - sx, py - sy) < SWORD_PICKUP_DIST:
+            swords.remove(sword)
+            has_sword = True
+            continue
+
+    for sx, sy in swords:
+        dx = sx - px
+        dy = sy - py
+        distance = math.hypot(dx, dy)
+
+        angle_to_sword = math.atan2(dy, dx)
+        delta_angle = angle_to_sword - angle
+
+        while delta_angle > math.pi:
+            delta_angle -= 2 * math.pi
+        while delta_angle < -math.pi:
+            delta_angle += 2 * math.pi
+
+        if -HALF_FOV < delta_angle < HALF_FOV and distance > 30:
+            projected_dist = distance * math.cos(delta_angle)
+            sword_size = min(350, int(50000 / projected_dist))
+
+            ray_index = int((delta_angle + HALF_FOV) / FOV * NUM_RAYS)
+            screen_x = ray_index * SCALE
+
+            if 0 <= ray_index < NUM_RAYS and projected_dist < zbuffer[ray_index]:
+                sword_img = pygame.transform.scale(
+                    sword_image, (sword_size, sword_size)
+                )
+
+                screen.blit(
+                    sword_img,
+                    (screen_x - sword_size // 2,
+                    HEIGHT // 2 + (HEIGHT // 4) - sword_size)
+                )
 
         #draw_centered_text("Press C for Credits", small_font, (200, 200, 200), HEIGHT - 20)
 
